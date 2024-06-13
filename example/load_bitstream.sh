12bcace08dbe9cb24261f4ae0d9dffff3d2d05b7
DouglasWWolf <douglaswolf@yahoo.com>
2024-06-13 01:52:38 -0700

    No commit message

--- example/load_bitstream.sh
+++ example/load_bitstream.sh
@@ -1,1 +1,1 @@
-load_bitstream -hot_reset -config load_jtag.conf -part xcku15p_0 -jtag_id $INDY_JTAG_ID indy_bc_emu.bit
+load_bitstream -hot_reset -config load_jtag.conf -part xcku15p_0 indy_bc_emu.bit
